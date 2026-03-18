e2ed893e406ed6f264765a2653a2daca540592ae
om29dev <om2bharambe29@gmail.com>
2026-03-18 09:11:14 +0530

    Remove old project files and restructure repository

--- server/check_coords.js
+++
@@ -1,9 +0,0 @@
-const { pool } = require('./db');
-async function checkCoords() {
-    try {
-        const res = await pool.query("SELECT id, lat, lon FROM pipeline_nodes LIMIT 5");
-        console.log("Raw Rows:", res.rows);
-    } catch (e) { console.error(e); }
-    pool.end();
-}
-checkCoords();
--- server/debug_edges.js
+++
@@ -1,9 +0,0 @@
-const { pool } = require('./db');
-async function run() {
-    try {
-        const id = 'inf_13382599924';
-        const res = await pool.query("SELECT * FROM pipeline_edges WHERE source_id = $1 OR target_id = $1", [id]);
-        console.log(`Edges for ${id}:`, res.rows);
-    } catch (e) { console.error(e); } finally { pool.end(); }
-}
-run();
